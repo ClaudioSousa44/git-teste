49dc388d8e6ec0061488dfda09ee62010bc28ce7
Claudio Sousa <cauhsousa@gmail.com>
2022-08-31 16:06:48 -0300

    mais um teste

--- src/br/senai/sp/jandira/Cliente.java
+++ src/br/senai/sp/jandira/Cliente.java
@@ -3,8 +3,12 @@ package br.senai.sp.jandira;
 public class Cliente {
 	public String nome;
 	public String email;
-	public String cidade;
+	
 	public int idade;
+	public int teste1;
+	public int teste2;
+	public int teste3;
+	public int teste4;
 		
 
 
